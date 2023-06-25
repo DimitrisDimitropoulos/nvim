42fba3e9fab2788d683a836e04c2734c414eaec0
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-06-26 01:00:15 +0300

    Blank_Line
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -4,9 +4,51 @@ require("telescope").setup({
     layout_config = {
       horizontal = {
         prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
       },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    preview = {
+      treesitter = false,
+      filesize_limit = 1,
+      timeout = 250,
+      -- truncate for preview
+      filesize_hook = function(filepath, bufnr, opts)
+        local path = require("plenary.path"):new(filepath)
+        -- opts exposes winid
+        local height = vim.api.nvim_win_get_height(opts.winid)
+        local lines = vim.split(path:head(height), "[\r]?\n")
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+      end,
+    },
+    vimgrep_arguments = {
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
     },
   },
+  mappings = {
+    n = { ["q"] = require("telescope.actions").close },
+  },
+  pickers = {
+    diagnostics = {
+      path_display = "hidden",
+    },
+  },
+  path_display = { "truncate" },
+  color_devicons = true,
+  set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
   extensions = {
     fzf = {
       fuzzy = true,                -- false will only do exact matching
