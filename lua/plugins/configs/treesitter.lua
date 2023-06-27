22c5624b120e97a174e3ce7e2d873d9b47dc68c1
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-06-27 17:43:52 +0300

    (feat) moved to zf from fzf

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -25,6 +25,10 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     use_languagetree = true,
+    additional_vim_regex_highlighting = false,
   },
-  indent = { enable = true },
+
+  indent = {
+    enable = true,
+  },
 })
