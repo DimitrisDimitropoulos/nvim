82b40d216f492c67ecbd2ae62e5067f817a67864
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-06-27 19:21:34 +0300

    (feat): cmp improved

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,5 +1,4 @@
 -- vim.keymap.set()
 local keymapp = vim.keymap.set
 local opts = { noremap = true, silent = false }
--- keymapp("n", "<leader>tc", "<cmd> AerialToggle<CR>", opts)
 keymapp("n", "<leader>tr", "<cmd> TroubleToggle <CR>", { desc = "diagnotics" }, opts)
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -31,4 +31,16 @@ require("nvim-treesitter.configs").setup({
   indent = {
     enable = true,
   },
+
+  refactor = {
+    highlight_definitions = { enable = true },
+    highlight_current_scope = { enable = true },
+
+    smart_rename = {
+      enarle = true,
+      keymaps = {
+        smart_rename = "grr",
+      },
+    },
+  },
 })
