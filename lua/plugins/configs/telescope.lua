794e1aac8828425d90a42c349ecb9973e2c0f191
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-06-27 17:43:52 +0300

    (feat) moved to zf from fzf
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -50,14 +50,19 @@ require("telescope").setup({
   color_devicons = true,
   set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
   extensions = {
-    fzf = {
-      fuzzy = true,                -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case",    -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
+    ["zf-native"] = {
+      file = {
+        enable = true,
+        highlight_results = true,
+        match_filename = true,
+      },
+      generic = {
+        enable = true,
+        highlight_results = true,
+        match_filename = false,
+      },
     },
   },
 })
 
-require("telescope").load_extension("fzf")
+require("telescope").load_extension("zf-native")
