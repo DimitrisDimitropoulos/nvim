28b49be550c5ff265703fc3244c39dfd952902c2
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-06-28 15:48:56 +0300

    (clean): heirline removed
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -1,12 +1,9 @@
-vim.api.nvim_create_augroup("AutoFormatting", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.jl",
-  group = "AutoFormatting",
-  callback = function()
-    vim.lsp.buf.format { async = true }
-  end,
-  desc = "format on save",
-})
-local keymapp = vim.keymap.set
-local opts = { noremap = true, silent = false }
-keymapp("n", "<leader>tr", "<cmd> TroubleToggle <CR>", { desc = "diagnotics" }, opts)
+-- vim.api.nvim_create_augroup("AutoFormatting", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.jl",
+--   group = "AutoFormatting",
+--   callback = function()
+--     vim.lsp.buf.format { async = true }
+--   end,
+--   desc = "format on save",
+-- })
--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,9 +1,1 @@
-vim.api.nvim_create_augroup("AutoFormatting", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.yaml",
-  group = "AutoFormatting",
-  callback = function()
-    vim.lsp.buf.format { async = true }
-  end,
-  desc = "format on save",
-})
+
