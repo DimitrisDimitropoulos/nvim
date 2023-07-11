cd88ebbc43d4be39bed26e3dfe7fe510c9099926
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-07-11 19:25:22 +0300

    Fixed ZZ ZQ-added copilot_cmp do not use
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -8,9 +8,9 @@ require("mason").setup({
     keymaps = {
       toggle_server_expand = "<CR>",
       install_server = "i",
-      update_server = "s",
+      update_server = "u",
       check_server_version = "c",
-      update_all_servers = "S",
+      update_all_servers = "U",
       check_outdated_servers = "C",
       uninstall_server = "X",
       cancel_installation = "<C-c>",
