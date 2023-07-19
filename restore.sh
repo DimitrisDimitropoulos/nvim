11fa2467be921c6be47a0ded0e0066b9fea12a6a
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-07-19 23:38:49 +0300

    (feature):Backup and restore scipts added for LaZy
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

---
+++ restore.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+# This script restores the lazy-lock.json file
+# to the last from the backup folder
+
+# Get the backup directory
+backup_dir="$HOME/.config/nvim/lockfiles"
+# Check if the directory exists
+if [ ! -d "$backup_dir" ]; then
+    echo "The backup directory $backup_dir does not exist!"
+fi
+
+# Get the last modified file
+newest_file=$(find "$backup_dir" -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d' ')
+
+if [ -f "$newest_file" ]; then
+    # Prompt the user to confirm before restoring the backup file
+    read -rp "Are you sure you want to restore $newest_file? (y/n) " choice
+    case "$choice" in
+        y|Y )
+            echo "Restoring $newest_file..."
+            pause 5
+            rm lazy-lock.json
+            echo "Removed the current lazy-lock.json file!"
+            cp "$newest_file" .
+            mv "$(basename "$newest_file")" lazy-lock.json
+            ;;
+        n|N )
+            echo "Aborted restore operation."
+            ;;
+        * )
+            echo "Invalid choice. Aborted restore operation."
+            ;;
+    esac
+else
+    echo "No backup files found!"
+    echo "Perhaps you could use git reset"
+fi
