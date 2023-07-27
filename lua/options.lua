8f51dc7e59ecf6d7822663cb7eef6f201f565358
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-07-27 15:13:44 +0300

    (feature): Diagnostic Signs

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,16 @@
 local opt = vim.opt
 local g = vim.g
 
+local signs = {
+  { hl = "DiagnosticSignError", txt = "■" },
+  { hl = "DiagnosticSignWarn", txt = "△" },
+  { hl = "DiagnosticSignInfo", txt = "○" },
+  { hl = "DiagnosticSignHint", txt = "󰨔" },
+}
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.hl, { text = sign.txt, texthl = sign.hl })
+end
+
 vim.diagnostic.config({
   underline = true,
   virtual_text = true,
