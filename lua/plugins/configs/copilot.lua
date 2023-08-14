93cd2ce98c0d2e2336a0aa5c1d666bfdea50a144
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-08-14 16:39:56 +0300

    (chore): added code actions to efm beta added copilot.lua
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -1,44 +1,35 @@
--- local present, copilot = pcall(require, "copilot")
---
--- require("copilot").setup {
---   panel = {
---     enabled = false,
---     auto_refresh = true,
---     keymap = {
---       jump_prev = "[[",
---       jump_next = "]]",
---       accept = "<CR>",
---       refresh = "gr",
---       open = "<M-CR>",
---     },
---     layout = {
---       position = "bottom", -- | top | left | right
---       ratio = 0.4,
---     },
---   },
---   suggestion = {
---     enabled = false,
---     auto_trigger = true,
---     debounce = 75,
---     keymap = {
---       accept = "<Tab>",
---       accept_word = true,
---       accept_line = true,
---       next = "<M-]>",
---       prev = "<M-[>",
---       dismiss = "<C-]>",
---     },
---   },
---   filetypes = {
---     yaml = false,
---     help = false,
---     gitcommit = false,
---     gitrebase = false,
---     hgcommit = false,
---     svn = false,
---     cvs = false,
---     ["."] = false,
---   },
---   copilot_node_command = "node", -- Node.js version must be > 16.x
---   server_opts_overrides = {},
--- }
+local copilot_ok, copilot = pcall(require, "copilot")
+if not copilot_ok then return end
+
+copilot.setup {
+  panel = {
+    enabled = false,
+    auto_refresh = false,
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<Tab>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+    help = false,
+    gitcommit = true,
+    gitrebase = true,
+    hgcommit = true,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = "node", -- Node.js version must be > 16.x
+  server_opts_overrides = {},
+}
