772e8285b7c0e3d4747b07098e70d5384afe54ae
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-08-15 18:59:21 +0300

    (chore): moved lsp config separatly from other plugins and cleaned it
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

---
+++ lua/plugins/configs/lsp/maps.lua
@@ -0,0 +1,38 @@
+local map = vim.keymap.set
+local lsp = vim.lsp.buf
+local lsp_mappings = {
+  { key = "gd", cmd = "definition",              desc = "goto def" },
+  { key = "gD", cmd = "declaration",             desc = "goto dec" },
+  { key = "gi", cmd = "implementation",          desc = "goto impl" },
+  { key = "ln", cmd = "rename",                  desc = "rename" },
+  { key = "kk", cmd = "signature_help",          desc = "signature" },
+  { key = "gr", cmd = "references",              desc = "references" },
+  { key = "gh", cmd = "type_definition",         desc = "type definition" },
+  { key = "wa", cmd = "add_workspace_folder",    desc = "add work folder" },
+  { key = "wr", cmd = "remove_workspace_folder", desc = "rm work folder" },
+}
+
+local diagno = {
+  { key = "<leader>df", cmd = "open_float", descr = "diagnostics float" },
+  { key = "<leader>ds", cmd = "show",       descr = "diagnostics show" },
+  { key = "<leader>dh", cmd = "hide",       descr = "diagnostics hide" },
+  { key = "[d",         cmd = "goto_prev",  descr = "diagnostics prev" },
+  { key = "]d",         cmd = "goto_next",  descr = "diagnostics next" },
+}
+
+map("n", "<S-k>", lsp.hover, { desc = "hover" })
+map("n", "<leader>qf", lsp.code_action, { desc = "code actions", silent = true })
+map("n", "<A-f>", function() lsp.format { async = true } end, { desc = "format code" })
+map(
+  "n",
+  "<space>wl",
+  function() print(vim.inspect(lsp.list_workspace_folders())) end,
+  { desc = "list workspace folders" }
+)
+for _, mapping in ipairs(lsp_mappings) do
+  map("n", "<leader>" .. mapping.key, lsp[mapping.cmd], { desc = mapping.desc })
+end
+
+for _, diag in ipairs(diagno) do
+  map("n", diag.key, vim.diagnostic[diag.cmd], { desc = diag.descr })
+end
---
+++ lua/plugins/configs/lsp/rust_analyzer.lua
@@ -0,0 +1,14 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local lspconfig = require "lspconfig"
+return {
+  lspconfig.rust_analyzer.setup {
+    capabilities = capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
