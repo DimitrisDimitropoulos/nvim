afb92351621c66c2bbc92fac6d2e0edfe2643f5f
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-08-16 15:48:19 +0300

    (feature): moved to heriline problems with lsp progress resolved
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/lsp/diagnostics.lua
+++ lua/plugins/configs/lsp/diagnostics.lua
@@ -1,9 +1,8 @@
--- NOTE: here follows the diagnostics config, @2023-08-11 14:35:14
 local signs = {
-  { hl = "DiagnosticSignError", txt = "■" },
-  { hl = "DiagnosticSignWarn", txt = "△" },
-  { hl = "DiagnosticSignInfo", txt = "○" },
-  { hl = "DiagnosticSignHint", txt = "󰨔" },
+  { hl = 'DiagnosticSignError', txt = '■' },
+  { hl = 'DiagnosticSignWarn', txt = '△' },
+  { hl = 'DiagnosticSignInfo', txt = '○' },
+  { hl = 'DiagnosticSignHint', txt = '󰨔' },
 }
 
 for _, sign in ipairs(signs) do
@@ -13,14 +12,13 @@ end
 vim.diagnostic.config {
   underline = true,
   virtual_text = {
-    prefix = "󰋙",
-    -- prefix = "󰄛",
+    prefix = ' ',
   },
   signs = true,
   update_in_insert = false,
   float = {
-    source = "always",
-    border = "rounded",
+    source = 'always',
+    border = 'rounded',
     show_header = true,
   },
 }
--- lua/plugins/configs/lsp/lua_ls.lua
+++ lua/plugins/configs/lsp/lua_ls.lua
@@ -1,21 +1,22 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-local lspconfig = require "lspconfig"
+local lspconfig = require 'lspconfig'
 lspconfig.lua_ls.setup {
   capabilities = capabilities,
   settings = {
     Lua = {
       runtime = {
-        version = "LuaJIT",
+        version = 'LuaJIT',
       },
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       hint = { enable = true }, -- only for nvim 10.0
       diagnostics = {
-        globals = { "vim" },
-        -- disable = { "different-requires" },
+        globals = { 'vim' },
       },
       format = {
-        enable = true,
+        -- if enabled will ruin the diagnostics, because it confuses it self,
+        -- use it only for tables, until stylua get that functionality
+        enable = false,
       },
     },
   },
