80827cbf0c43e435fe5b03927197e21e2831b376
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2023-09-01 23:44:28 +0300

    remove prefix in efm and latex support

--- lua/plugins/configs/lsp/efm.lua
+++ lua/plugins/configs/lsp/efm.lua
@@ -1,6 +1,5 @@
 ---@diagnostic disable: unused-local
 local prettier = {
-  prefix = 'prettier',
   formatCanRange = true,
   formatCommand = (
     'prettier --stdin --stdin-filepath ${INPUT} ${--range-start:charStart} '
@@ -19,10 +18,10 @@ local prettier = {
     '.prettierrc.toml',
   },
 }
-local gersemi = { prefix = 'gersemi', formatCommand = 'gersemi -', formatStdin = true }
-local fourmolu = { prefix = 'fourmolu', formatCommand = 'fourmolu --stdin-input-file ${INPUT} -', formatStdin = true }
-local beautysh = { prefix = 'beautysh', formatCommand = 'beautysh -', formatStdin = true }
-local black = { prefix = 'black', formatCommand = 'black --fast -', formatStdin = true }
+local gersemi = { formatCommand = 'gersemi -', formatStdin = true }
+local fourmolu = { formatCommand = 'fourmolu --stdin-input-file ${INPUT} -', formatStdin = true }
+local beautysh = { formatCommand = 'beautysh -', formatStdin = true }
+local black = { formatCommand = 'black --fast -', formatStdin = true }
 local pylint = {
   prefix = 'pylint',
   lintSource = 'pylint',
@@ -31,7 +30,6 @@ local pylint = {
   lintFormats = { '%.%#:%l:%c: %t%.%#: %m' },
 }
 local stylua = {
-  prefix = 'stylua',
   formatCommand = 'stylua --search-parent-directories --stdin-filepath ${INPUT} -',
   formatStdin = true,
   rootMarkers = { 'stylua.toml', '.stylua.toml' },
@@ -77,16 +75,8 @@ local jq_lint = {
   lintOffset = 1,
   lintFormats = { '%m at line %l, column %c' },
 }
-local jq_format = {
-  prefix = 'jq',
-  formatCommand = 'jq',
-  formatStdin = true,
-}
-local shfmt = {
-  prefix = 'shfmt',
-  formatCommand = 'shfmt -filename ${INPUT} -',
-  formatStdin = true,
-}
+local jq_format = { formatCommand = 'jq', formatStdin = true }
+local shfmt = { formatCommand = 'shfmt -filename ${INPUT} -', formatStdin = true }
 local selene = {
   prefix = 'selene',
   lintSource = 'selene',
@@ -104,7 +94,7 @@ local chktex = {
   lintFormats = { '%f:%l:%c:%n:%m' },
   lintSeverity = 2,
 }
-local latexindent = { prefix = 'latexindent', formatCommand = 'latexindent -', formatStdin = true }
+local latexindent = { formatCommand = 'latexindent -', formatStdin = true }
 
 -- -- FIXME: ruff, @2023-08-29 17:16:40
 -- local ruff = {
