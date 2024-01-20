817a13dccb582a9d35e723d881f607b7277c8b69
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-01-20 02:33:07 +0200

    chore: changes to texlab and fix luasnip for friendly
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/lsp/texlab.lua
+++ lua/plugins/configs/lsp/texlab.lua
@@ -1,21 +1,40 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local lspconfig = require 'lspconfig'
 
+-- local util = require 'lspconfig.util'
+-- local function buf_cancel_build()
+--   local texlab_client = util.get_active_client_by_name(0, 'texlab')
+--   if texlab_client then
+--     texlab_client.request('workspace/executeCommand', { command = 'texlab.cancelBuild' }, function(err)
+--       if err then error(tostring(err)) end
+--       print 'Build cancelled'
+--     end, 0)
+--   end
+-- end
+
 return {
   -- NOTE: with the following config it can replace null-ls and vimtex, @2023-08-11 15:29:27
   lspconfig.texlab.setup {
     capabilities = capabilities,
     settings = {
       texlab = {
+        experimental = {
+          verbatimEnvironments = { 'minted', 'lstlisting' },
+          mathEnvironments = { 'cases', 'equation', 'equation*', 'align', 'align*' },
+          enumEnvironments = { 'enumerate', 'itemize', 'description' },
+          citationCommands = { 'textcite', 'cite', 'parencite', 'supercite', 'autocite' },
+        },
         build = {
           auxDirectory = 'build',
           logDirectory = 'build',
+          pdfDirectory = 'build',
           onSave = true,
           args = {
             '-pdf',
             '-lualatex',
             '-interaction=nonstopmode',
             '-aux-directory=build',
+            '-output-directory=build',
             '-synctex=1',
             '%f',
           },
@@ -35,7 +54,7 @@ return {
           modifyLineBreaks = false,
         },
         bibtexFormatter = 'texlab',
-        formatterLineLength = 80,
+        formatterLineLength = 120,
       },
     },
   },
