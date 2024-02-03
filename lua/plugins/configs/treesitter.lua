523f4b3c4ec31400bb66994faea6b98ab374c00f
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-02-03 19:50:15 +0200

    fix: remove treesitter highlight groups
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -9,13 +9,3 @@ require('nvim-treesitter.configs').setup {
   refactor = { highlight_definitions = { enable = false }, highlight_current_scope = { enable = false } },
   autopairs = { enable = false },
 }
-
--- hl groups for the comments
--- NOTE: Treesitter is prone to breaking changes, @2023-07-24 17:05:41
-local comms_hl = { '@text.todo', '@text.danger', '@text.warning', '@text.note' }
-for _, hl in ipairs(comms_hl) do
-  vim.api.nvim_set_hl(0, hl, { bold = true, underline = true })
-end
-
-vim.api.nvim_set_hl(0, 'VioletHLGroup', { fg = '#d4bfff' })
-vim.api.nvim_set_hl(0, '@lsp.typemod.keyword', { link = 'VioletHLGroup' })
