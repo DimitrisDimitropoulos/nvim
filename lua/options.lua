57d14048c0484813fb8ac2b8d9d493fd78450624
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-02-13 22:53:09 +0200

    chore: remove if for xclip use only winyank for windows

--- lua/options.lua
+++ lua/options.lua
@@ -22,15 +22,6 @@ opt.cursorline = true
 opt.cursorcolumn = false
 opt.clipboard = 'unnamedplus'
 
-if vim.fn.has 'unix' == 1 then
-  if vim.fn.executable 'xclip' == 1 then
-    vim.g.clipboard = {
-      copy = { ['+'] = 'xclip -selection clipboard', ['*'] = 'xclip -selection clipboard' },
-      paste = { ['+'] = 'xclip -selection clipboard -o', ['*'] = 'xclip -selection clipboard -o' },
-    }
-  end
-end
-
 if vim.fn.has 'win32' == 1 or vim.fn.has 'wsl' == 1 then
   vim.g.clipboard = {
     copy = { ['+'] = 'win32yank.exe -i --crlf', ['*'] = 'win32yank.exe -i --crlf' },
