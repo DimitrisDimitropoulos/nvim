5fe98b5f53ae28ce738f884a12085194352212d0
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-02-14 17:18:54 +0200

    feat: make default shell pwsh

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ' '
 
 local opt = vim.opt
 
+opt.shell = 'pwsh.exe'
+
 opt.langmap =
   'ΑA,ΒB,ΨC,ΔD,ΕE,ΦF,ΓG,ΗH,ΙI,ΞJ,ΚK,ΛL,ΜM,ΝN,ΟO,ΠP,QQ,ΡR,ΣS,ΤT,ΘU,ΩV,WW,ΧX,ΥY,ΖZ,αa,βb,ψc,δd,εe,φf,γg,ηh,ιi,ξj,κk,λl,μm,νn,οo,πp,qq,ρr,σs,τt,θu,ωv,ςw,χx,υy,ζz'
 
