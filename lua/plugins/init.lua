68386d2c20dfc1e5112b6ffa79958178a4605466
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-06-18 17:15:46 +0300

    feat: disable alpha nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,7 @@ local plugins = {
 
   -- { 'savq/melange-nvim', lazy = false, config = function() vim.cmd.colorscheme 'melange' end },
   { 'folke/tokyonight.nvim', lazy = false, config = function() vim.cmd.colorscheme 'tokyonight-storm' end },
-  { 'goolord/alpha-nvim', event = 'VimEnter', config = function() require 'plugins.configs.alpha' end },
+  -- { 'goolord/alpha-nvim', event = 'VimEnter', config = function() require 'plugins.configs.alpha' end },
   { 'rebelot/heirline.nvim', event = 'VeryLazy', config = function() require 'plugins.configs.heirline' end },
   { 'nvim-tree/nvim-web-devicons', config = function() require('nvim-web-devicons').setup() end },
 
