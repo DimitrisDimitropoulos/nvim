7b2948e7137842d50027344809a5303f468dd8e5
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-06-20 22:05:51 +0300

    chore: delete empty lines from alpha.nvim

--- lua/plugins/configs/alpha.lua
+++ lua/plugins/configs/alpha.lua
@@ -3,7 +3,6 @@ if not alpha_ok then return end
 local dashboard = require 'alpha.themes.dashboard'
 
 dashboard.section.header.opts = { hl = 'Comment', position = 'center' }
-
 dashboard.section.header.val = {
   ' ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
   ' ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
@@ -25,18 +24,15 @@ dashboard.section.buttons.val = {
 }
 
 dashboard.section.footer.opts = { hl = 'PeanutHLGroup', position = 'center' }
-
 local function footer()
   local stats = require('lazy').stats()
   local version = vim.version()
   local nvim_version_info = '    v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
   return '  ⚡Plugins ' .. stats.count .. nvim_version_info
 end
-
 dashboard.section.footer.val = footer()
 
 vim.opt_local.fillchars = { eob = ' ' }
-
 alpha.setup(dashboard.opts)
 
 vim.api.nvim_set_hl(0, 'VioletHLGroup', { fg = '#d4bfff' })
