8f44fd21eaf6e67da103a4a7e39c7814fd565e71
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-03 19:31:19 +0300

    chore: remove unused all ft snippets and snippet_utils functions

--- snippets/lua_snippets/all.lua
+++ snippets/lua_snippets/all.lua
@@ -5,12 +5,7 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
-local extras = require 'luasnip.extras'
-local l = extras.lambda
-local p = extras.partial
 local fmt = require('luasnip.extras.fmt').fmt
-
-local sn_ut = require 'utils.snippet_utils'
 
 return {
 
@@ -38,41 +33,4 @@ return {
       end),
     })
   ),
-
-  s({
-    trig = 'sepcomm',
-    name = 'Comment-Separate',
-    dscr = 'Separate with commentstring',
-  }, f(sn_ut.separate_with_commentstring)),
-
-  s({
-    trig = 'sepeq',
-    name = 'Equals-Separate',
-    dscr = 'Separate with equals',
-  }, f(sn_ut.separate_with_equals)),
-
-  s(
-    {
-      trig = 'titlesep',
-      name = 'Title-Separate',
-      dscr = 'Separate with title',
-    },
-    fmt('{}\n{}{}{} {}\n{}', {
-      f(sn_ut.separate_with_equals),
-      f(sn_ut.get_comment_string),
-      f(sn_ut.get_comment_string),
-      i(1, 'Title'),
-      f(sn_ut.get_comment_string),
-      f(sn_ut.separate_with_equals),
-    })
-  ),
-
-  s('time', p(vim.fn.strftime, '%H:%M:%S')),
-  s('shrug', { t '¯\\_(ツ)_/¯' }),
-  s('lenny', { t '( ͡° ͜ʖ ͡°)' }),
-  s('tableflip', { t '(╯°□°）╯︵ ┻━┻' }),
-  s('tableback', { t '┬─┬ ノ( ゜-゜ノ)' }),
-  s('happy', { t 'ヽ(´▽`)/' }),
-  s('sad', { t '(╥_╥)' }),
-  s('confused', { t '(｡･ω･｡)' }),
 }
