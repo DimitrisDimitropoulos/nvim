3d9a3a56c5a78fbb329848be5e9d25da693b33bc
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-03 17:34:48 +0300

    chore: update general mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,18 +4,13 @@ map({ 'i', 'n' }, '<C-s>', function()
   vim.cmd.write {}
 end)
 map('t', '<C-[><C-[>', '<C-\\><C-n>', { silent = true })
-map('i', '<C-H>', '<C-w>')
-map('i', '<C-BS>', '<C-w>')
 
-map('n', '<leader>bd', function()
-  vim.cmd 'bd'
-end, { noremap = true, desc = 'delete buffer' })
-map('n', '<leader>bn', function()
-  vim.cmd 'bn'
-end, { noremap = true, desc = 'next buffer' })
-map('n', '<leader>bp', function()
-  vim.cmd 'bp'
-end, { noremap = true, desc = 'previous buffer' })
+local buf = { 'bd', 'bn', 'bp' }
+for _, b in ipairs(buf) do
+  map('n', '<leader>' .. b, function()
+    vim.cmd(b)
+  end, { noremap = true, desc = b })
+end
 
 local arrows = { '<Up>', '<Down>', '<Left>', '<Right>' }
 for _, arrow in ipairs(arrows) do
@@ -25,9 +20,3 @@ end
 map('n', '<leader>mm', function()
   vim.cmd 'make'
 end, { noremap = true, desc = 'make' })
-map('n', '<leader>mr', function()
-  vim.cmd 'make run'
-end, { noremap = true, desc = 'make run' })
-map('n', '<leader>mc', function()
-  vim.cmd 'make clean'
-end, { noremap = true, desc = 'make clean' })
