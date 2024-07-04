075af1212c598f6fb17d39ca516f82c8cba2ceb2
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-05 01:49:29 +0300

    chore: reformat the lsp and diagnostics mappings

--- lua/plugins/configs/lsp/maps.lua
+++ lua/plugins/configs/lsp/maps.lua
@@ -1,43 +1,39 @@
 local map = vim.keymap.set
 local lsp = vim.lsp.buf
 local lsp_mappings = {
-  -- stylua: ignore start
-  { key = 'gi', cmd = 'implementation',          desc = 'goto impl' },
-  { key = 'kk', cmd = 'signature_help',          desc = 'signature' },
-  { key = 'gh', cmd = 'type_definition',         desc = 'type definition' },
-  { key = 'wa', cmd = 'add_workspace_folder',    desc = 'add work folder' },
-  { key = 'wr', cmd = 'remove_workspace_folder', desc = 'rm work folder' },
-  -- stylua: ignore stop
+  { key = 'gi', cmd = 'implementation' },
+  { key = 'kk', cmd = 'signature_help' },
+  { key = 'gh', cmd = 'type_definition' },
+  { key = 'wa', cmd = 'add_workspace_folder' },
+  { key = 'wr', cmd = 'remove_workspace_folder' },
 }
 
 local diagno = {
-  -- stylua: ignore start
-  { key = '<leader>ds', cmd = 'show',      descr = 'diagnostics show' },
-  { key = '<leader>dh', cmd = 'hide',      descr = 'diagnostics hide' },
-  { key = '<leader>dl', cmd = 'setqflist', descr = 'diagnostics to loclist' },
-  -- stylua: ignore stop
+  { key = '<leader>ds', cmd = 'show' },
+  { key = '<leader>dh', cmd = 'hide' },
+  { key = '<leader>dl', cmd = 'setqflist' },
 }
 
-map('n', 'grr', lsp.references, { desc = 'references' })
-map('n', 'grn', lsp.rename, { desc = 'rename' })
-map('n', 'gra', lsp.code_action, { desc = 'code actions', silent = true })
+map('n', 'grr', lsp.references, { desc = 'lsp references' })
+map('n', 'grn', lsp.rename, { desc = 'lsp rename' })
+map('n', 'gra', lsp.code_action, { desc = 'lsp code actions', silent = true })
 map('n', '<leader>lh', function()
   if vim.lsp.inlay_hint.is_enabled() then
     vim.lsp.inlay_hint.enable(false)
   else
     vim.lsp.inlay_hint.enable(true)
   end
-end, { desc = 'toggle inlay hints' })
+end, { desc = 'lsp toggle inlay hints' })
 map('n', '<A-f>', function()
   lsp.format { async = true }
-end, { desc = 'async format' })
+end, { desc = 'lsp async format' })
 map('n', '<space>wl', function()
   print(vim.inspect(lsp.list_workspace_folders()))
-end, { desc = 'list workspace folders' })
+end, { desc = 'lsp list workspace folders' })
 for _, mapping in ipairs(lsp_mappings) do
-  map('n', '<leader>' .. mapping.key, lsp[mapping.cmd], { desc = mapping.desc })
+  map('n', '<leader>' .. mapping.key, lsp[mapping.cmd], { desc = 'lsp ' .. mapping.cmd:gsub('_', ' ') })
 end
 
 for _, diag in ipairs(diagno) do
-  map('n', diag.key, vim.diagnostic[diag.cmd], { desc = diag.descr })
+  map('n', diag.key, vim.diagnostic[diag.cmd], { desc = 'diagnostics ' .. diag.cmd:gsub('_', ' ') })
 end
