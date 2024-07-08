0b01c8cfd1a857adf860971abdd292869b5fcf4e
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-08 17:08:36 +0300

    feat: add cmp-nvim-lsp-signature-help source

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,6 +68,7 @@ local plugins = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'saadparwaiz1/cmp_luasnip',
       -- 'hrsh7th/cmp-nvim-lua',
 
