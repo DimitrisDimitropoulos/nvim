3d2730f7647671f4f26080b5ed2228c9b6e92c12
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-14 14:09:25 +0300

    chore: disable auto inlay hints
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/lsp/init.lua
+++ lua/plugins/configs/lsp/init.lua
@@ -1,9 +1,11 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-      vim.lsp.inlay_hint.enable()
+    if false then
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+        vim.lsp.inlay_hint.enable()
+      end
     end
     vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
     vim.lsp.handlers['textDocument/signatureHelp'] =
