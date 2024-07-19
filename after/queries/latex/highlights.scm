af51f4455096b4ab45014b1d7cc6dd5038ce1260
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-19 20:13:38 +0300

    feat: add queries highlights for latex highlight left right commands

---
+++ after/queries/latex/highlights.scm
@@ -0,0 +1,8 @@
+; extends
+
+(math_delimiter
+  left_command: _ @punctuation.delimiter
+  left_delimiter: _ @punctuation.delimiter
+  right_command: _ @punctuation.delimiter
+  right_delimiter: _ @punctuation.delimiter
+)
