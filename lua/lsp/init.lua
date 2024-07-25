eecff20b6cbead9e8793c34f4f01c754b1fa23f4
DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-07-25 12:45:29 +0300

    feat: add completion autotrigger feature

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,6 +2,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(args)
     if false then
+      vim.lsp.completion.enable(true, args.data.client_id, args.buf, { autotrigger = true })
+    end
+    if false then
       local client = vim.lsp.get_client_by_id(args.data.client_id)
       if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
         vim.lsp.inlay_hint.enable()
