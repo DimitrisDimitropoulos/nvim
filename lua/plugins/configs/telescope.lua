21e607c8de9a0824f7dfa5a8d99feeaa2eafbbda
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:43:04 +0300

    telescope symbol
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -10,6 +10,7 @@ local previewers = require 'telescope.previewers'
 
 telescope.setup {
   defaults = {
+    prompt_prefix = '   ',
     file_previewer = previewers.vim_buffer_cat.new,
     grep_previewer = previewers.vim_buffer_vimgrep.new,
     qflist_previewer = previewers.vim_buffer_qflist.new,
