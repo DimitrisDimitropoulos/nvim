79e11b50bfca127697d6851699e35c169d071ef8
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:25 +0300

    chore: use vim.uv instead of vim.loop
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ require 'commands'
 require 'lsp'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
