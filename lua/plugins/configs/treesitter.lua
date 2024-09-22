827b0ec04c4747f1745d96083b15a50f5209d726
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:24 +0300

    chore(treesitter): no need to ensure_installed they are included to core
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,4 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'c', 'vimdoc', 'markdown', 'markdown_inline' },
   highlight = {
     enable = true,
     use_languagetree = true,
