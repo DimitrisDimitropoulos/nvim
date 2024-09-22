85720b8890b392603c2785309081b9ee6431dacc
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:42:49 +0300

    (chore): Clean unused ftplugin files
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- after/ftplugin/context.lua
+++ after/ftplugin/context.lua
@@ -1,3 +1,3 @@
-local keymapp = vim.api.nvim_set_keymap
-keymapp("n", "<leader>lf", "<cmd>silent !context %:r.tex; zathura %:r.pdf & <CR>", { desc = "Fompile" })
-keymapp("n", "<C-s>", "<cmd>w <CR>| :silent !context %:r.tex; zathura -e %:r.pdf & <CR>", { desc = "Compile" })
+local map = vim.api.nvim_set_keymap
+map("n", "<leader>lf", "<cmd>silent !context %:r.tex; zathura %:r.pdf & <CR>", { desc = "Fompile" })
+map("n", "<C-s>", "<cmd>w <CR>| :silent !context %:r.tex; zathura -e %:r.pdf & <CR>", { desc = "Compile" })
