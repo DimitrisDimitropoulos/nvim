868cae59054b6fb3df1cd44be354942b0f1e5d04
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:42:45 +0300

    (fix):Using default ZZ ZQ no silent
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/other.lua
+++ lua/plugins/configs/other.lua
@@ -21,30 +21,30 @@ require("indent_blankline").setup {
 
 -- Mini textobjects
 require("mini.ai").setup {
-  custom_textobjects = {
-    -- Whole buffer textobject
-    M = function(ai_type)
-      local n_lines = vim.fn.line "$"
-      local start_line, end_line = 1, n_lines
-      if ai_type == "i" then
-        -- Skip first and last blank lines for `i` textobject
-        local first_nonblank, last_nonblank = vim.fn.nextnonblank(1), vim.fn.prevnonblank(n_lines)
-        start_line = first_nonblank == 0 and 1 or first_nonblank
-        end_line = last_nonblank == 0 and n_lines or last_nonblank
-      end
-      local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-      return {
-        from = {
-          line = start_line,
-          col = 1,
-        },
-        to = {
-          line = end_line,
-          col = to_col,
-        },
-      }
-    end,
-  },
+  -- custom_textobjects = {
+  --   -- Whole buffer textobject
+  --   M = function(ai_type)
+  --     local n_lines = vim.fn.line "$"
+  --     local start_line, end_line = 1, n_lines
+  --     if ai_type == "i" then
+  --       -- Skip first and last blank lines for `i` textobject
+  --       local first_nonblank, last_nonblank = vim.fn.nextnonblank(1), vim.fn.prevnonblank(n_lines)
+  --       start_line = first_nonblank == 0 and 1 or first_nonblank
+  --       end_line = last_nonblank == 0 and n_lines or last_nonblank
+  --     end
+  --     local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  --     return {
+  --       from = {
+  --         line = start_line,
+  --         col = 1,
+  --       },
+  --       to = {
+  --         line = end_line,
+  --         col = to_col,
+  --       },
+  --     }
+  --   end,
+  -- },
 }
 -- Mini splitjoins arguments no TS required
 require("mini.splitjoin").setup {}
