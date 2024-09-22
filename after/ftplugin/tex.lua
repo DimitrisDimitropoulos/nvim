cc221f6cc278e80b01a802d6fd3cdb07a910657e
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:43:13 +0300

    chore: change tex setting error in syntax spell toplevel
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -3,5 +3,8 @@ vim.opt_local.spell = true
 vim.opt_local.wrap = true
 
 local function moving_wrap(direction) return (vim.v.count == 0) and 'g' .. direction or direction end
-vim.keymap.set({ 'n', 'x' }, '<Up>', function() return moving_wrap 'k' end, { expr = true, silent = true })
-vim.keymap.set({ 'n', 'x' }, '<Down>', function() return moving_wrap 'j' end, { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'k', function() return moving_wrap 'k' end, { expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'j', function() return moving_wrap 'j' end, { expr = true, silent = true })
+
+-- BUG: For some reason this does not work, @2024-01-25 01:51:09
+vim.cmd.syntax 'spell toplevel'
