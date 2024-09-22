9970a72a5f615db1336b8dc8434c3f2cc0e77dc6
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:41 +0300

    fix(mason): update default installations
    Signed-off-by: Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -1,15 +1,9 @@
 return {
   ui = { icons = { package_installed = '', package_pending = '', package_uninstalled = '' } },
   vim.api.nvim_create_user_command('MasonInstallAll', function()
-    vim.cmd('MasonInstall ' .. table.concat({
-      'lua-language-server',
-      'bash-language-server',
-      'shellcheck',
-      'efm',
-      'ruff',
-      'prettier',
-      'texlab',
-    }, ' '))
+    vim.cmd(
+      'MasonInstall ' .. table.concat({ 'lua-language-server', 'stylua', 'efm', 'ruff', 'prettier', 'texlab' }, ' ')
+    )
   end, {}),
   PATH = 'skip',
   max_concurrent_installers = 20,
