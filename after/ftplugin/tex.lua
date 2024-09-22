d1baea5a8a8ce8112688effefbe8810748d1e86e
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:39 +0300

    chore(tex): drop command moved to core
    Signed-off-by: Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -22,49 +22,6 @@ end, { silent = false, noremap = true, desc = 'toggle greek keymap' })
 -- BUG: For some reason this does not work, @2024-01-25 01:51:09
 vim.cmd.syntax 'spell toplevel'
 
-local util = require 'lspconfig.util'
-local function buf_find_envs(bufnr)
-  bufnr = util.validate_bufnr(bufnr)
-  local texlab_client = util.get_active_client_by_name(bufnr, 'texlab')
-  if not texlab_client then
-    return vim.notify('Texlab client not found', vim.log.levels.ERROR)
-  end
-  local pos = vim.api.nvim_win_get_cursor(0)
-  texlab_client.request('workspace/executeCommand', {
-    command = 'texlab.findEnvironments',
-    arguments = {
-      {
-        textDocument = { uri = vim.uri_from_bufnr(bufnr) },
-        position = { line = pos[1] - 1, character = pos[2] },
-      },
-    },
-  }, function(err, result)
-    if err then
-      return vim.notify(err.code .. ': ' .. err.message, vim.log.levels.ERROR)
-    end
-    local env_names = {}
-    local max_length = 1
-    for _, env in ipairs(result) do
-      table.insert(env_names, env.name.text)
-      max_length = math.max(max_length, string.len(env.name.text))
-    end
-    for i, name in ipairs(env_names) do
-      env_names[i] = string.rep(' ', i - 1) .. name
-    end
-    vim.lsp.util.open_floating_preview(env_names, '', {
-      height = #env_names,
-      width = math.max((max_length + #env_names - 1), (string.len 'Environments')),
-      focusable = false,
-      focus = false,
-      border = vim.g.border_style,
-      title = 'Environments',
-    })
-  end, bufnr)
-end
-vim.api.nvim_create_user_command('TexlabPrettyEnvs', function()
-  buf_find_envs(0)
-end, { nargs = 0, desc = 'Find environments at cursor and display as popup' })
-
 local bufnr = vim.api.nvim_get_current_buf() ---@type number
 local function get_labels()
   local parser = vim.treesitter.get_parser(bufnr, 'latex') ---@type vim.treesitter.LanguageTree
