ab25b17141b5ce0ebbff88de7fc4d9fc60dba7bd
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:30 +0300

    feat(tex): add gO TOC for latex
    Create a treesitter based way to find all headings and pipe the into
    loclist
    
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -121,3 +121,109 @@ end
 vim.api.nvim_create_user_command('Fzflatexlabels', function()
   fzf_label()
 end, { nargs = 0, desc = 'FzfLua get LaTeX labels based on treesitter' })
+
+---@brief
+-- Extracts all LaTeX section headings from the current buffer.
+--
+-- This function uses Tree-sitter to parse the current buffer and identify
+-- various LaTeX sectioning commands such as `part`, `chapter`, `section`,
+-- `subsection`, etc. It constructs and executes queries for each sectioning
+-- command to locate the headings in the document.
+--
+---@return table: A list of heading entries, where each entry is a table with the following keys:
+--   - `type` (string): The type of sectioning command (e.g., 'section').
+--   - `text` (string): The text of the heading.
+--   - `line` (integer): The 1-based line number of the heading.
+--   - `start` (integer): The starting column of the heading text.
+--   - `path` (string): The path of the current buffer.
+local function get_headings()
+  local parser = vim.treesitter.get_parser(bufnr, 'latex') ---@type vim.treesitter.LanguageTree
+  local root = parser:parse()[1]:root() ---@type table<integer, TSTree>
+  -- List of LaTeX sectioning commands
+  local headings = {}
+  for _, command in ipairs {
+    'part',
+    'chapter',
+    'section',
+    'subsection',
+    'subsubsection',
+    'paragraph',
+    'subparagraph',
+  } do
+    -- Construct a query for each LaTeX sectioning command
+    local query_string = string.format('(%s (curly_group (text) @heading_title))', command)
+    local query = vim.treesitter.query.parse('latex', query_string)
+    for _, match, _ in query:iter_matches(root, bufnr, 0, -1) do
+      for _, node in pairs(match) do
+        local text = vim.treesitter.get_node_text(node, bufnr) ---@type string
+        local line, start, _, _ = node:range() ---@type integer, integer, integer, integer
+        line = line + 1
+        local entry = {
+          type = command,
+          text = text,
+          line = line,
+          start = start,
+          path = vim.api.nvim_buf_get_name(0), ---@type string
+        }
+        table.insert(headings, entry)
+      end
+    end
+  end
+  return headings
+end
+
+---@brief
+-- Generates and displays a table of contents (TOC) for the current LaTeX buffer.
+--
+-- This function checks if a location list window is already open for the
+-- current buffer. If so, it reopens the location list. Otherwise, it creates a
+-- new location list populated with headings extracted from the LaTeX document.
+-- It also sets a title for the location list.
+--
+---@return nil
+--
+-- This function does not return any value. It updates the location list window
+-- with TOC entries and ensures that the list is associated with the current
+-- buffer.
+--
+---@see get_headings: This function is used to fetch the headings from the current LaTeX document.
+local function show_toc()
+  local bufname = vim.fn.bufname '%' ---@type string
+  -- Get location list info
+  local info = vim.fn.getloclist(0, { winid = 1 })
+  -- Check if loclist is already open and corresponds to the current buffer
+  if info.winid ~= 0 and vim.api.nvim_win_get_var(info.winid, 'qf_toc') == bufname then
+    vim.cmd 'lopen'
+    return
+  end
+  -- Generate TOC from headings
+  local toc = {}
+  for _, heading in ipairs(get_headings()) do
+    table.insert(toc, {
+      bufnr = bufnr,
+      lnum = heading.line, ---@type integer
+      col = heading.start + 1, ---@type integer
+      -- Capitalize the first letter of the heading type
+      text = string.format('%s: %s', string.upper(heading.type:sub(1, 1)) .. heading.type:sub(2), heading.text),
+    })
+  end
+  -- Set the location list with TOC entries
+  vim.fn.setloclist(0, toc)
+  -- Set the title for the location list
+  local title = 'LaTeX TOC'
+  if vim.g.document_title then
+    title = title .. ' - "' .. vim.g.document_title .. '"'
+  end
+
+  -- Update the location list with the title
+  vim.fn.setloclist(0, {}, 'r', { title = title })
+  vim.cmd 'lopen'
+  -- Set a variable in the location list window to identify it as the TOC for this buffer
+  local winid = vim.fn.getloclist(0, { winid = 1 }).winid
+  if winid ~= 0 then
+    vim.api.nvim_win_set_var(winid, 'qf_toc', bufname)
+  end
+end
+vim.keymap.set('n', 'gO', function()
+  show_toc()
+end, { silent = true, noremap = true, desc = 'User: show LaTeX TOC' })
