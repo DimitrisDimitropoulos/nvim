cca77e28d1c71450fac82ccf8385c423d738596a
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:44:31 +0300

    chore: refactor single use variables
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -88,9 +88,7 @@ local plugins = {
         'windwp/nvim-autopairs',
         config = function()
           require('nvim-autopairs').setup()
-          local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-          local cmp = require 'cmp'
-          cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+          require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
         end,
       },
     },
