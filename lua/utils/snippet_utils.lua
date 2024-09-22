87643d7adf5e7b1126c6c7d4d0e6d2b2629d13c3
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-09-22 13:42:56 +0300

    (fix): cmp sorting, cannot add mypy to efm
    Signed-off-by: DimitrisDimitropoulos <dimitris.dimitropoulos00@gmail.com>

--- lua/utils/snippet_utils.lua
+++ lua/utils/snippet_utils.lua
@@ -6,16 +6,35 @@ function M.get_comment_string()
   return commentstring:gsub("%%s", "")
 end
 
--- now i want to append 80 times this character
 function M.separate_with_commentstring()
   local commentstring = tostring(M.get_comment_string())
   -- remove trailing whitespace
   commentstring = commentstring:gsub("%s+$", "")
-  -- check the number of characters in order to
-  -- append the correct amount
   if #commentstring == 1 then return commentstring .. string.rep(commentstring, 79) end
   return commentstring .. string.rep(commentstring, 39)
   -- NOTE: lua numbering begins @1, @2023-07-24 17:13:23
+end
+
+function M.separate_with_equals()
+  local commentstring = tostring(M.get_comment_string())
+  -- remove trailing whitespace
+  commentstring = commentstring:gsub("%s+$", "")
+  if #commentstring == 1 then return commentstring .. " " .. string.rep("=", 78) end
+  return commentstring .. " " .. string.rep("=", 77)
+end
+
+function M.center_word(word)
+  word = tostring(word)
+  -- i want to center a word
+  local commentstring = tostring(M.get_comment_string())
+  -- remove trailing whitespace
+  commentstring = commentstring:gsub("%s+$", "")
+  if #commentstring == 1 then
+    local len = 1 * math.floor((79 - #word) / 2) - 3
+    return commentstring .. " " .. string.rep("=", len) .. word .. string.rep("=", len)
+  end
+  local len = 1 * math.floor((77 - #word) / 2) - 4
+  return commentstring .. " " .. string.rep("=", len) .. " " .. word .. " " .. string.rep("=", len)
 end
 
 return M
