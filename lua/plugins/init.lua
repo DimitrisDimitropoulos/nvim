65c869e1bdae282ad0e96055651e4b28f21129ba
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-10-03 18:08:35 +0300

    chore(cmp): remove signature help source

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,7 +44,6 @@ require('lazy').setup({
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
       'saadparwaiz1/cmp_luasnip',
       -- 'hrsh7th/cmp-nvim-lua',
 
