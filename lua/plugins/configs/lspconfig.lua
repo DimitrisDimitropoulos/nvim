f77bd79244ed85febc5c3a7b1bc90722cc1d43f7
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-11-09 00:03:35 +0200

    feat(lspconfig): add basedpyright

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require('lspconfig.ui.windows').default_options.border = vim.g.border_style
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local lspconfig = require 'lspconfig'
-for _, lsp in ipairs { 'julials', 'bashls', 'neocmake', 'clangd', 'ruff', 'taplo', 'typst_lsp', 'yamlls' } do
+for _, lsp in ipairs { 'julials', 'bashls', 'neocmake', 'clangd', 'ruff', 'taplo', 'typst_lsp', 'yamlls', 'basedpyright' } do
   lspconfig[lsp].setup { capabilities = capabilities }
 end
 for _, lsp in ipairs { 'texlab', 'rust_analyzer', 'efm', 'lua_ls' } do
