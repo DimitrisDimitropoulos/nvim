f8aabb53886ba7f7dc05bbe2526edb1d43211ec8
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2024-12-21 00:55:19 +0200

    chore(cmp): remove nvim-lua source completely

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,7 +47,6 @@ require('lazy').setup({
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
       { 'saadparwaiz1/cmp_luasnip', enabled = false },
-      -- 'hrsh7th/cmp-nvim-lua',
 
       {
         'L3MON4D3/LuaSnip',
