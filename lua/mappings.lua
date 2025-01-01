bb863789dd9c8049315629436e47c7455c7091b4
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2025-01-02 01:25:16 +0200

    fix(mappings): move closer to core style

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,16 +1,22 @@
 vim.keymap.set('n', '<C-s>', vim.cmd.write)
 
-for _, b in ipairs { 'bd', 'bn', 'bp' } do
-  vim.keymap.set('n', '<leader>' .. b, function()
-    vim.cmd(b)
-  end, { noremap = true })
+if vim.version().minor < 11 then
+  vim.keymap.set('n', '[b', function()
+    vim.cmd 'bp'
+  end, { noremap = true, desc = 'previous buffer' })
+  vim.keymap.set('n', ']b', function()
+    vim.cmd 'bn'
+  end, { noremap = true, desc = 'next buffer' })
+  vim.keymap.set('n', '[q', function()
+    vim.cmd 'cprev'
+  end, { noremap = true, desc = 'previous quickfix' })
+  vim.keymap.set('n', ']q', function()
+    vim.cmd 'cnext'
+  end, { noremap = true, desc = 'next quickfix' })
 end
-vim.keymap.set('n', '<leader>cn', function()
-  vim.cmd 'cn'
-end, { noremap = true })
-vim.keymap.set('n', '<leader>cp', function()
-  vim.cmd 'cp'
-end, { noremap = true })
+vim.keymap.set('n', '<leader>bd', function()
+  vim.cmd 'bd'
+end, { noremap = true, desc = 'delete buffer' })
 
 for _, arrow in ipairs { '<Up>', '<Down>', '<Left>', '<Right>' } do
   vim.keymap.set({ 'n', 'x', 'c' }, arrow, '')
