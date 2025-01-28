b9016f35315567bdf846e9f757e3f415db93fa1d
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2025-01-28 17:46:31 +0200

    feat(typst): implement get labels-section commands

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_user_command('GetTypstLabels', function()
+  require('utils').gen_loclist(require('utils').get_entries('((label) @label)', 'typst'), 'Typst Labels')
+end, { nargs = 0, desc = 'Generate a location list with Typst labels' })
+
+vim.api.nvim_create_user_command('GetTypstHeadings', function()
+  require('utils').gen_loclist(
+    require('utils').get_entries('((section (heading (text) @text)))', 'typst'),
+    'Typst Headings'
+  )
+end, { nargs = 0, desc = 'Generate a location list with Typst headings' })
+
 ---@param command_name string
 local function create_tinymist_command(command_name)
   local export_type = command_name:match 'tinymist%.export(%w+)'
