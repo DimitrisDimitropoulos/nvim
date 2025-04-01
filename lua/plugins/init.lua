0f4fb3e0a3a4ba52ceec79225f6d9cb6e334b82f
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2025-04-02 00:02:34 +0300

    chore: lazy config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -185,4 +185,35 @@ require('lazy').setup({
     cmd = 'HighlightColors',
     opts = {},
   },
-}, require 'plugins.configs.lazy')
+}, {
+  install = { colorscheme = { 'melange' } },
+  defaults = { lazy = true },
+  rocks = { enabled = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        '2html_plugin',
+        'tohtml',
+        'getscript',
+        'getscriptPlugin',
+        'gzip',
+        'netrw',
+        'netrwPlugin',
+        'netrwSettings',
+        'netrwFileHandlers',
+        'tar',
+        'tarPlugin',
+        'spellfile_plugin',
+        'zip',
+        'zipPlugin',
+        'tutor',
+        'rplugin',
+        'syntax',
+        'synmenu',
+        'optwin',
+        'bugreport',
+        'matchit',
+      },
+    },
+  },
+})
