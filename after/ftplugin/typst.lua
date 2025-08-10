0059cbc5968b45e2bd70b7d91b004f75697a7d4b
Dimitris Dimitropoulos <dimitris.dimitropoulos00@gmail.com>
2025-08-10 13:06:50 +0300

    fix(typst): toc with gO keymap

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -2,9 +2,9 @@ vim.api.nvim_create_user_command('GetTypstLabels', function()
   require('utils').gen_loclist(require('utils').get_entries('((label) @label)', 'typst'), 'Typst Labels')
 end, { nargs = 0, desc = 'Generate a location list with Typst labels' })
 
-vim.api.nvim_create_user_command('GetTypstHeadings', function()
+vim.keymap.set('n', 'gO', function()
   require('utils').gen_loclist(
     require('utils').get_entries('((section (heading (text) @text)))', 'typst'),
     'Typst Headings'
   )
-end, { nargs = 0, desc = 'Generate a location list with Typst headings' })
+end, { silent = true, noremap = true, desc = 'User: show LaTeX TOC' })
